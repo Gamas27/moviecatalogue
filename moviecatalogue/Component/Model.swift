b6886b2d3c5fd97585e6843ed7a52fb980418bd0
João Gameiro <gamas27@gmail.com>
2022-09-05 17:22:39 +0100

    Create models for the API Response

--- moviecatalogue/Component/Model.swift
+++ moviecatalogue/Component/Model.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct Welcome: Decodable {
+    let searchType, expression: String
+    let results: [Result]
+    let errorMessage: String
+}
+
+// MARK: - Result
+struct Result: Decodable {
+    let id, resultType: String
+    let image: String
+    let title, resultDescription: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, resultType, image, title
+        case resultDescription = "description"
+    }
+
+    func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+        }
+
+        public static func == (lhs: Result, rhs: Result) -> Bool {
+            return lhs.id == rhs.id
+        }
+}
